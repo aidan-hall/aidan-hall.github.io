4b421f1d6401538e37b326a798daa31edad2e47a
Aidan Hall <aidan.hall@outlook.com>
2025-11-09 22:33:06 +0000

    .dir-locals.el: Added

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((org-id-link-to-org-use-id . nil)
+         (org-id-link-consider-parent-id . nil)
+         (org-link-file-path-type . relative)
+         (org-export-async-init-file . "~/Documents/websites/orgsite/publish-config.el")))
+ (org-mode . ((auto-insert-alist . ((org-mode "Title: " "#+title: " str "\n" "#+date: "
+                                              '(org-timestamp-inactive '(16)) "\n"))))))
